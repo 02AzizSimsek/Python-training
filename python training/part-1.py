4a587432c75989a3151b31a7f01e490c976ac504
aziz şimşek <98578225+02AzizSimsek@users.noreply.github.com>
2023-02-25 21:54:49 +0300

    Add files via upload

---
+++ python training/part-1.py
@@ -0,0 +1,120 @@
+"""
+###############
+
+print(3+3)
+print("3"+"3")
+print(type(3))
+print(type(3.6))
+print(type(6.0))
+print(type(0.0))
+print(type(3+3.8))
+print(type(4+4.0))
+
+###############
+
+#Math Operations
+
+3+4
+4.0+3
+2*2
+4.0*2
+13/3
+10/2
+print(type(10/2))
+4+6*2
+(2+6)*2
+
+3**2
+36**0.5
+10%3
+10/3
+10//3
+84/8
+84//8
+(13//5)**2
+(13//5)**2*6
+
+###############
+
+# Variables
+
+# 7000 +%18 or 8000+%18
+
+print(7000+(7000*0.18))
+print(8000+(8000*0.18))
+
+#we have defined a variable
+product1=2000
+product2=8500
+print(product1 +(product1 * 0.18))
+print(product2+ (product2*0.15))
+
+phone=12000
+notebook=18500
+tax=0.18
+pricePhone=phone + (phone*tax)
+priceNote=notebook+(notebook*tax)
+print("The price of the phone in Turkey:",pricePhone)
+print("The price of the notebook in Turkey:",priceNote)
+
+###############
+
+a,b,c=10,440,3
+print(a,b,c)
+
+x=1
+print(type(x))
+y=3.4
+print(type(y))
+name='aziz'
+print(type(name))
+
+isStudent=True
+
+print(type(isStudent))
+
+a,b,name,isStudent=10,40,"aziz",True
+print(a,b,name,isStudent)
+
+###############
+"""
+#Data Types and Transformation
+
+age=28
+weight=55.6
+name="aziz"
+isStudent=True
+
+print(type(age))
+print(type(weight))
+print(type(name))
+print(type(isStudent))
+
+# int to float
+
+result=float(age)
+print(result)
+
+#float to int
+
+result=int(weight)
+print(result)
+
+#bool to str
+
+result=str(isStudent)
+print(result)
+print(type(result))
+
+#bool to result
+
+result=int(isStudent)
+print(result)
+print(type(result))
+
+
+
+
+
+
+
